4b4a3d552130120c2c047abeeaf268f60d1ff19a
Víctor Sánchez Grande <107065114+victorsgDev@users.noreply.github.com>
2022-11-30 21:07:20 +0100

    insert detalle pendiente

---
+++ controller/db_controller.py
@@ -0,0 +1,35 @@
+from db.db_logic import Db_Logic as db  # from modulo.archivo import class
+
+
+class Db_controller:
+    database = db()
+
+    def add_categoria(self, data):
+        self.database.add_categoria(data)
+
+    def add_cliente(self, data):
+        self.database.add_cliente(data)
+
+    # def delete_client(self, id):
+    #     self.database.delete_cliente_by_id(id)
+
+    def add_pedido(self, data):
+        self.database.add_pedido(data)
+
+    def add_detalle(self, data):
+        self.database.add_detalle(data)
+
+    def add_producto(self, data):
+        self.database.add_producto(data)
+
+    def update_cliente(self, data):
+        self.database.update_cliente(data)
+
+    def update_categoria(self, data):
+        self.database.update_categoria(data)
+
+    def update_pedido(self, data):
+        self.database.update_pedido(data)
+
+    def update_producto(self, data):
+        self.database.update_producto(data)
--- ui/main.py
+++ ui/main.py
@@ -1,10 +1,13 @@
 import tkinter as tk
 from tkinter import ttk
+from tkinter import messagebox as mb
+
+from controller.db_controller import Db_controller as Controller
 
 
-# Clase
 class FormularioTablas:
     def __init__(self):
+        self.Controller = Controller()
         self.ventana1 = tk.Tk()
         self.ventana1.title("Gestión de Supermercado")
         self.cuaderno1 = ttk.Notebook(self.ventana1)
@@ -35,7 +38,8 @@ class FormularioTablas:
         self.entry_cat = ttk.Entry(self.labelframe1, textvariable=self.cat_tipo)
         self.entry_cat.grid(column=1, row=1, padx=4, pady=4)
 
-        self.boton1 = ttk.Button(self.labelframe1, text="Añadir Registro a Categoría", command=self.add_categoria())
+        self.boton1 = ttk.Button(self.labelframe1, text="Añadir Registro a Categoría",
+                                 command=self.agregar_cat)
         self.boton1.grid(column=1, row=2, padx=4, pady=4)
 
     # Formulario tabla cliente
@@ -47,9 +51,9 @@ class FormularioTablas:
 
         self.label1 = ttk.Label(self.labelframe1, text="IdCliente:")
         self.label1.grid(column=0, row=0, padx=4, pady=4)
-        self.idpedido_tipo = tk.IntVar()
-        self.entry_idpedido = ttk.Entry(self.labelframe1, textvariable=self.idpedido_tipo)
-        self.entry_idpedido.grid(column=1, row=0, padx=4, pady=4)
+        self.idcliente_tipo = tk.StringVar()
+        self.entry_idcliente = ttk.Entry(self.labelframe1, textvariable=self.idcliente_tipo)
+        self.entry_idcliente.grid(column=1, row=0, padx=4, pady=4)
 
         self.label2 = ttk.Label(self.labelframe1, text="Cia:")
         self.label2.grid(column=0, row=1, padx=4, pady=4)
@@ -111,9 +115,12 @@ class FormularioTablas:
         self.entry_fax = ttk.Entry(self.labelframe1, textvariable=self.fax_tipo)
         self.entry_fax.grid(column=1, row=11, padx=4, pady=4)
 
-        self.boton1 = ttk.Button(self.labelframe1, text="Añadir Registro a Cliente", command=self.add_cliente())
+        self.boton1 = ttk.Button(self.labelframe1, text="Añadir Registro a Cliente", command=self.agregar_cli)
         self.boton1.grid(column=1, row=12, padx=4, pady=4)
         self.boton2 = ttk.Button(self.labelframe1, text="Borrar Cliente por ID")
+
+        self.boton3 = ttk.Button(self.labelframe1, text="Actualizar a Cliente", command=self.update_cli)
+        self.boton3.grid(column=1, row=13, padx=4, pady=4)
 
     # Formulario tabla pedido
     def form_pedido(self):
@@ -124,24 +131,31 @@ class FormularioTablas:
 
         self.label1 = ttk.Label(self.labelframe1, text="Id pedido:")
         self.label1.grid(column=0, row=0, padx=4, pady=4)
-        self.idpedido_tipo = tk.IntVar()
+        self.idpedido_tipo = tk.StringVar()
         self.entry_idpedido = ttk.Entry(self.labelframe1, textvariable=self.idpedido_tipo)
         self.entry_idpedido.grid(column=1, row=0, padx=4, pady=4)
 
+        self.label1 = ttk.Label(self.labelframe1, text="Id cliente:")
+        self.label1.grid(column=0, row=1, padx=4, pady=4)
+        self.idcliente_tipo = tk.StringVar()
+        self.entry_idcliente = ttk.Entry(self.labelframe1, textvariable=self.idcliente_tipo)
+        self.entry_idcliente.grid(column=1, row=1, padx=4, pady=4)
+
         self.label2 = ttk.Label(self.labelframe1, text="Fecha pedido:")
-        self.label2.grid(column=0, row=1, padx=4, pady=4)
+        self.label2.grid(column=0, row=2, padx=4, pady=4)
         self.fechaP_tipo = tk.StringVar()
         self.entry_fechaP = ttk.Entry(self.labelframe1, textvariable=self.fechaP_tipo)
-        self.entry_fechaP.grid(column=1, row=1, padx=4, pady=4)
+        self.entry_fechaP.grid(column=1, row=2, padx=4, pady=4)
 
         self.label2 = ttk.Label(self.labelframe1, text="Fecha entrega:")
-        self.label2.grid(column=0, row=2, padx=4, pady=4)
+        self.label2.grid(column=0, row=3, padx=4, pady=4)
         self.fechaE_tipo = tk.StringVar()
         self.entry_fechaE = ttk.Entry(self.labelframe1, textvariable=self.fechaE_tipo)
-        self.entry_fechaE.grid(column=1, row=2, padx=4, pady=4)
+        self.entry_fechaE.grid(column=1, row=3, padx=4, pady=4)
 
-        self.boton1 = ttk.Button(self.labelframe1, text="Añadir Registro a Pedido", command=self.add_pedido())
-        self.boton1.grid(column=1, row=12, padx=4, pady=4)
+        self.boton1 = ttk.Button(self.labelframe1, text="Añadir Registro a Pedido",
+                                 command=self.agregar_pedido)  # ! importante llamar a las funcineos
+        self.boton1.grid(column=1, row=4, padx=4, pady=4)
         self.boton2 = ttk.Button(self.labelframe1, text="Borrar Pedido por ID")
 
     # Formulario tabla detalle
@@ -153,44 +167,38 @@ class FormularioTablas:
 
         self.label1 = ttk.Label(self.labelframe1, text="Id pedido:")
         self.label1.grid(column=0, row=0, padx=4, pady=4)
-        self.idpedido_tipo = tk.IntVar()
-        self.entry_idpedido = ttk.Entry(self.labelframe1, textvariable=self.idpedido_tipo)
-        self.entry_idpedido.grid(column=1, row=0, padx=4, pady=4)
+        self.iddetalle = tk.IntVar()
+        self.iddetalle_entry = ttk.Entry(self.labelframe1, textvariable=self.iddetalle)
+        self.iddetalle_entry.grid(column=1, row=0, padx=4, pady=4)
 
-        self.label1 = ttk.Label(self.labelframe1, text="Id cliente:")
+        self.label1 = ttk.Label(self.labelframe1, text="Id Producto:")
         self.label1.grid(column=0, row=1, padx=4, pady=4)
-        self.idcliente_tipo = tk.IntVar()
-        self.entry_idcliente = ttk.Entry(self.labelframe1, textvariable=self.idcliente_tipo)
-        self.entry_idcliente.grid(column=1, row=1, padx=4, pady=4)
-
-        self.label1 = ttk.Label(self.labelframe1, text="Id pedido:")
-        self.label1.grid(column=0, row=2, padx=4, pady=4)
-        self.idpedido_tipo = tk.IntVar()
-        self.entry_idpedido = ttk.Entry(self.labelframe1, textvariable=self.idpedido_tipo)
-        self.entry_idpedido.grid(column=1, row=2, padx=4, pady=4)
+        self.idproducto_tipo = tk.IntVar()
+        self.entry_idproducto = ttk.Entry(self.labelframe1, textvariable=self.idproducto_tipo)
+        self.entry_idproducto.grid(column=1, row=1, padx=4, pady=4)
 
         self.label2 = ttk.Label(self.labelframe1, text="Precio")
-        self.label2.grid(column=0, row=3, padx=4, pady=4)
-        self.precio_tipo = tk.StringVar()
-        self.entry_precio = ttk.Entry(self.labelframe1, textvariable=self.precio_tipo)
-        self.entry_precio.grid(column=1, row=3, padx=4, pady=4)
+        self.label2.grid(column=0, row=2, padx=4, pady=4)
+        self.precioD_tipo = tk.DoubleVar()
+        self.entry_precioD = ttk.Entry(self.labelframe1, textvariable=self.precioD_tipo)
+        self.entry_precioD.grid(column=1, row=2, padx=4, pady=4)
 
         self.label2 = ttk.Label(self.labelframe1, text="Unidades")
-        self.label2.grid(column=0, row=4, padx=4, pady=4)
-        self.unidades_tipo = tk.StringVar()
+        self.label2.grid(column=0, row=3, padx=4, pady=4)
+        self.unidades_tipo = tk.IntVar()
         self.entry_unidades = ttk.Entry(self.labelframe1, textvariable=self.unidades_tipo)
-        self.entry_unidades.grid(column=1, row=4, padx=4, pady=4)
+        self.entry_unidades.grid(column=1, row=3, padx=4, pady=4)
 
         self.label2 = ttk.Label(self.labelframe1, text="Descuento")
-        self.label2.grid(column=0, row=5, padx=4, pady=4)
-        self.descuento_tipo = tk.StringVar()
+        self.label2.grid(column=0, row=4, padx=4, pady=4)
+        self.descuento_tipo = tk.IntVar()
         self.entry_descuento = ttk.Entry(self.labelframe1, textvariable=self.descuento_tipo)
-        self.entry_descuento.grid(column=1, row=5, padx=4, pady=4)
+        self.entry_descuento.grid(column=1, row=4, padx=4, pady=4)
 
-        self.botonRegistro = ttk.Button(self.labelframe1, text="Añadir Registro a Detalle", command=self.add_detalle())
-        self.botonRegistro.grid(column=1, row=12, padx=4, pady=4)
+        self.botonRegistro = ttk.Button(self.labelframe1, text="Añadir Registro a Detalle", command=self.agregar_detalle)
+        self.botonRegistro.grid(column=1, row=5, padx=4, pady=4)
         self.botonBorrarDetalle = ttk.Button(self.labelframe1, text="Borrar Detalle por ID")
-        self.botonBorrarDetalle.grid(column=1, row=13, padx=4, pady=4)
+        self.botonBorrarDetalle.grid(column=1, row=6, padx=4, pady=4)
 
     # Formulario tabla productos
     def tabla_productos(self):
@@ -199,57 +207,158 @@ class FormularioTablas:
         self.labelFrame1 = ttk.LabelFrame(self.paginaProductos, text="Producto")
         self.labelFrame1.grid(column=0, row=0, padx=5, pady=10)
 
-        self.label1 = ttk.Label(self.labelFrame1, text="Nombre: ")
+        self.label1 = ttk.Label(self.labelFrame1, text="id Producto: ")
         self.label1.grid(column=0, row=0, padx=4, pady=4)
+        self.id_producto = tk.StringVar()
+        self.id_producto_input = ttk.Entry(self.labelFrame1, textvariable=self.id_producto)
+        self.id_producto_input.grid(column=1, row=0, padx=4, pady=4)
+
+        self.label1 = ttk.Label(self.labelFrame1, text="Nombre: ")
+        self.label1.grid(column=0, row=1, padx=4, pady=4)
         self.nombre = tk.StringVar()
         self.nombreInput = ttk.Entry(self.labelFrame1, textvariable=self.nombre)
-        self.nombreInput.grid(column=1, row=0, padx=4, pady=4)
-
-        self.label1 = ttk.Label(self.labelFrame1, text="Id Categoria: ")
-        self.label1.grid(column=0, row=1, padx=4, pady=4)
-        self.id_categoria = tk.IntVar()
-        self.nombreInput = ttk.Entry(self.labelFrame1, textvariable=self.id_categoria)
         self.nombreInput.grid(column=1, row=1, padx=4, pady=4)
 
-        self.label1 = ttk.Label(self.labelFrame1, text="Medida: ")
+        self.label1 = ttk.Label(self.labelFrame1, text="Id Categoria: ")
         self.label1.grid(column=0, row=2, padx=4, pady=4)
-        self.medida = tk.StringVar()
-        self.nombreInput = ttk.Entry(self.labelFrame1, textvariable=self.medida)
+        self.id_categoria = tk.IntVar()
+        self.nombreInput = ttk.Entry(self.labelFrame1, textvariable=self.id_categoria)
         self.nombreInput.grid(column=1, row=2, padx=4, pady=4)
 
-        self.label1 = ttk.Label(self.labelFrame1, text="Precio: ")
+        self.label1 = ttk.Label(self.labelFrame1, text="Medida: ")
         self.label1.grid(column=0, row=3, padx=4, pady=4)
-        self.precio = tk.IntVar()
-        self.nombreInput = ttk.Entry(self.labelFrame1, textvariable=self.precio)
+        self.medida = tk.StringVar()
+        self.nombreInput = ttk.Entry(self.labelFrame1, textvariable=self.medida)
         self.nombreInput.grid(column=1, row=3, padx=4, pady=4)
 
-        self.label1 = ttk.Label(self.labelFrame1, text="Stock: ")
+        self.label1 = ttk.Label(self.labelFrame1, text="Precio: ")
         self.label1.grid(column=0, row=4, padx=4, pady=4)
-        self.stock = tk.IntVar()
-        self.nombreInput = ttk.Entry(self.labelFrame1, textvariable=self.stock)
+        self.precio = tk.IntVar()
+        self.nombreInput = ttk.Entry(self.labelFrame1, textvariable=self.precio)
         self.nombreInput.grid(column=1, row=4, padx=4, pady=4)
 
+        self.label1 = ttk.Label(self.labelFrame1, text="Stock: ")
+        self.label1.grid(column=0, row=5, padx=5, pady=4)
+        self.stock = tk.IntVar()
+        self.nombreInput = ttk.Entry(self.labelFrame1, textvariable=self.stock)
+        self.nombreInput.grid(column=1, row=5, padx=4, pady=4)
+
+        self.boton3 = ttk.Button(self.labelFrame1, text="Añadir a Producto",
+                                 command=self.agregar_producto)
+        self.boton3.grid(column=0, row=6, padx=4, pady=4)
+
         self.boton1 = ttk.Button(self.labelFrame1, text="Obtener gráfica")  # todo:  falta agregar command a ejecutar!
-        self.boton1.grid(column=0, row=5, padx=4, pady=4)
+        self.boton1.grid(column=0, row=7, padx=4, pady=4)
 
         self.boton2 = ttk.Button(self.labelFrame1, text="Obtener Excel")  # todo:  falta agregar command a ejecutar!
-        self.boton2.grid(column=1, row=5, padx=4, pady=4)
+        self.boton2.grid(column=1, row=7, padx=4, pady=4)
 
-        # todo: Falta hacer tabs de crear tabla y eliminar tabla!
 
-    # TODO MÉTODOS AÑADIR REGISTROS:
 
-    def add_categoria(self):
-        pass
+    def agregar_cli(self):
+        datos = (self.idcliente_tipo.get(), self.cia_tipo.get(), self.contacto_tipo.get(), self.cargo_tipo.get(),
+                 self.direccion_tipo.get(), self.ciudad_tipo.get(), self.region_tipo.get(), self.cp_tipo.get(),
+                 self.pais_tipo.get(), self.tlf_tipo.get(), self.fax_tipo.get())
+        mb.showinfo("Información", "Cliente añadido")
+        self.Controller.add_cliente(datos)
+        self.idcliente_tipo.set("")
+        self.cia_tipo.set("")
+        self.contacto_tipo.set("")
+        self.cargo_tipo.set("")
+        self.direccion_tipo.set("")
+        self.ciudad_tipo.set("")
+        self.region_tipo.set("")
+        self.cp_tipo.set("")
+        self.pais_tipo.set("")
+        self.tlf_tipo.set("")
+        self.fax_tipo.set("")
 
-    def add_cliente(self):
-        pass
+    # def delete_cli(self):
+    #     datos = self.idcliente_tipo.get()
+    #     self.Controller.delete_client(datos)
+    #     mb.showinfo("Información", "Cliente eliminado")
+    #     self.idcliente_tipo.set(0)
 
-    def add_pedido(self):
-        pass
+    def agregar_cat(self):
+        datos = (self.idcat_tipo.get(), self.cat_tipo.get())
+        self.Controller.add_categoria(datos)
+        mb.showinfo("Información", "Categoría Añadida")
+        self.idcat_tipo.set(0)
+        self.cat_tipo.set("")
 
-    def add_detalle(self):
-        pass
+    def agregar_pedido(self):
+        datos = (self.idpedido_tipo.get(), self.idcliente_tipo.get(), self.fechaP_tipo.get(), self.fechaE_tipo.get())
+        self.Controller.add_pedido(datos)
+        mb.showinfo("Información", "Pedido Añadido")
+        self.idpedido_tipo.set(0)
+        self.idcliente_tipo.set(0)
+        self.fechaP_tipo.set("")
+        self.fechaE_tipo.set("")
+
+    def agregar_detalle(self):
+        datos = (self.iddetalle.get(), self.idproducto_tipo.get(), self.precioD_tipo.get(), self.unidades_tipo,
+                 self.descuento_tipo)
+        self.Controller.add_detalle(datos)
+        mb.showinfo("Información", "Detalle Añadido")
+        self.iddetalle.set(0)
+        self.idproducto_tipo.set(0)
+        self.precioD_tipo.set(0)
+        self.unidades_tipo.set(0)
+        self.descuento_tipo.set(0)
+
+    def agregar_producto(self):
+        datos = (
+            self.id_producto.get(),
+            self.idproducto_tipo.get(),
+            self.nombre.get(),
+            self.id_categoria.get(),
+            self.medida.get(),
+            self.precio.get(),
+            self.stock.get())
+
+        self.Controller.add_producto(datos)
+        mb.showinfo("Información", "Producto Añadido")
+        self.idproducto_tipo.set("")
+        self.nombre.set("")
+        self.id_categoria.set("")
+        self.medida.set("")
+        self.precio.set("")
+        self.stock.set("")
+
+    def update_cli(self):
+        datos = (self.idcliente_tipo.get(), self.cia_tipo.get(), self.contacto_tipo.get(), self.cargo_tipo.get(),
+                 self.direccion_tipo.get(), self.ciudad_tipo.get(), self.region_tipo.get(), self.cp_tipo.get(),
+                 self.pais_tipo.get(), self.tlf_tipo.get(), self.fax_tipo.get())
+
+        mb.showinfo("Información", "Cliente actualizado")
+        self.Controller.update_cliente(datos)
+        self.idcliente_tipo.set("")
+        self.cia_tipo.set("")
+        self.contacto_tipo.set("")
+        self.cargo_tipo.set("")
+        self.direccion_tipo.set("")
+        self.ciudad_tipo.set("")
+        self.region_tipo.set("")
+        self.cp_tipo.set("")
+        self.pais_tipo.set("")
+        self.tlf_tipo.set("")
+        self.fax_tipo.set("")
+
+    def update_categoria(self):
+        datos = (self.idcat_tipo.get(), self.cat_tipo.get())
+        self.Controller.update_categoria(datos)
+        mb.showinfo("Información", "Categoría actualizada")
+        self.idcat_tipo.set(0)
+        self.cat_tipo.set("")
+
+    def update_pedido(self):
+        datos = (self.idpedido_tipo.get(), self.idcliente_tipo.get(), self.fechaP_tipo.get(), self.fechaE_tipo.get())
+        self.Controller.add_pedido(datos)
+        mb.showinfo("Información", "Pedido actualizado")
+        self.idpedido_tipo.set(0)
+        self.idcliente_tipo.set(0)
+        self.fechaP_tipo.set("")
+        self.fechaE_tipo.set("")
 
 
 aplicacion1 = FormularioTablas()
